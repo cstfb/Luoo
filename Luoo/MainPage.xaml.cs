3c3a50b10e68c99206f568a2d93e1740285c7f87
田丰波 <fengbo.tian@tqmall.com>
2017-12-15 17:01:24 +0800

    init
    初始化

---
+++ Luoo/MainPage.xaml.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Luoo.Resources;
+using System.Windows.Media.Imaging;
+using Coding4Fun.Toolkit.Controls;
+using Microsoft.Phone.BackgroundAudio;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using Microsoft.Phone.Net.NetworkInformation;
+
+namespace Luoo
+{
+	public partial class MainPage : PhoneApplicationPage
+	{
+		#region 字段
+		static bool isAlbumChanged=true;
+		static int currentAlbumNumber=-1;
+		static int maxAlbumNumber=-1;
+		static bool isExit=false;
+		#endregion
+
+		#region 构造函数
+		public MainPage()
+		{
+			isExit=false;
+
+			InitializeComponent();
+			BuildLocalizedApplicationBar();
+
+			BackgroundAudioPlayer.Instance.PlayStateChanged+=Instance_PlayStateChanged;
+
+			SystemTray.SetIsVisible(this , true);
+			SystemTray.SetOpacity(this , 0);
+
+
+		}
+		#endregion
+
+		#region 应用程序栏
+
+		private void BuildLocalizedApplicationBar()
+		{
+			ApplicationBar = new ApplicationBar();
+			ApplicationBar.Mode=ApplicationBarMode.Minimized;
+			ApplicationBar.IsVisible=true;
+
+			ApplicationBarMenuItem callItem = new ApplicationBarMenuItem(AppResources.CallLuoo);
+			callItem.Click+=callItem_Click;
+
+			ApplicationBarMenuItem aboutItem=new ApplicationBarMenuItem(AppResources.About);
+			aboutItem.Click+=aboutItem_Click;
+
+			ApplicationBarMenuItem changeBackgroundItem=new ApplicationBarMenuItem(AppResources.ChangeBackground);
+			changeBackgroundItem.Click+=changeBackgroundItem_Click;
+
+			ApplicationBar.MenuItems.Add(callItem);
+			ApplicationBar.MenuItems.Add(changeBackgroundItem);
+			ApplicationBar.MenuItems.Add(aboutItem);
+		}
+		void changeBackgroundItem_Click( object sender , EventArgs e )
+		{
+			BitmapImage bC=backgroundImage.Source as BitmapImage;
+			if(bC.UriSource.ToString().Equals("assets/Tiles/background3.png"))
+			{
+				backgroundImage.Source=new BitmapImage(new Uri("assets/Tiles/background4.png" , UriKind.Relative));
+			}
+			else backgroundImage.Source=new BitmapImage(new Uri("assets/Tiles/background3.png" , UriKind.Relative));
+		}
+
+		private void aboutItem_Click( object sender , EventArgs e )
+		{
+			AboutPrompt aboutPrompt=new AboutPrompt();
+			aboutPrompt.VersionNumber="version:1.0";
+			aboutPrompt.Title="关于Luoo";
+			aboutPrompt.Show("XiaoTian" , "" , "tian_feng_bo@live.com" , @"http://www.cnblogs.com/au-xiaotian/");
+		}
+
+		void callItem_Click( object sender , EventArgs e )
+		{
+			Windows.System.Launcher.LaunchUriAsync(new System.Uri("http://www.luoo.net"));
+		}
+
+		#endregion
+
+		#region 播放控制
+
+		private void playAndPauseButton_Click( object sender , RoutedEventArgs e )
+		{
+			playAndPauseStoryBoard.Begin();
+			UnableAllButton();
+			BitmapImage bI=playAndPause.Source as BitmapImage;
+			if(bI.UriSource.ToString().Equals("/assets/icons/play.png"))
+			{
+				playAndPause.Source=new BitmapImage(new Uri("/assets/icons/pause.png" , UriKind.Relative));
+				BackgroundAudioPlayer.Instance.Play();
+			}
+			else
+			{
+				playAndPause.Source=new BitmapImage(new Uri("/assets/icons/play.png" , UriKind.Relative));
+				BackgroundAudioPlayer.Instance.Pause();
+			}
+			EnableAllButton();
+
+		}
+
+		private void lastAlbumButton_Click( object sender , RoutedEventArgs e )
+		{
+			lastAlbumStoryBoard.Begin();
+			progressBar.Visibility=Visibility.Visible;
+			UnableAllButton();
+			BackgroundAudioPlayer.Instance.Rewind();
+			isAlbumChanged=true;
+		}
+
+		private void nextAlbumButton_Click( object sender , RoutedEventArgs e )
+		{
+			//判断是否有下一期
+			if(currentAlbumNumber<maxAlbumNumber)
+			{
+				nextAlbumStoryBoard.Begin();
+				progressBar.Visibility=Visibility.Visible;
+				UnableAllButton();
+				BackgroundAudioPlayer.Instance.FastForward();
+				isAlbumChanged=true;
+			}
+		}
+
+		private void nextSongButton_Click( object sender , RoutedEventArgs e )
+		{
+			nextSongStoryBoard.Begin();
+			progressBar.Visibility=Visibility.Visible;
+			UnableAllButton();
+			BackgroundAudioPlayer.Instance.SkipNext();
+		}
+
+		private void lastSongButton_Click( object sender , RoutedEventArgs e )
+		{
+			lastSongStoryBoard.Begin();
+			progressBar.Visibility=Visibility.Visible;
+			UnableAllButton();
+			BackgroundAudioPlayer.Instance.SkipPrevious();
+		}
+		#endregion
+
+		#region UI Control
+		private void UpdateUI()
+		{
+			if(BackgroundAudioPlayer.Instance.PlayerState==PlayState.Playing)
+			{
+				playAndPause.Source=new BitmapImage(new Uri("/assets/icons/pause.png" , UriKind.Relative));
+			}
+			else
+			{
+				playAndPause.Source=new BitmapImage(new Uri("/assets/icons/play.png" , UriKind.Relative));
+			}
+
+			var t=BackgroundAudioPlayer.Instance.Track;
+			if(isAlbumChanged)
+			{
+				//set albumCover
+				albumCover.Opacity=0;
+				albumCover.ImageSource=new BitmapImage(t.AlbumArt);
+				//图像淡入
+				albumCover.ImageOpened+=albumCover_ImageOpened;
+			}
+			//set album
+			album.Text=t.Album;
+			//set artist
+			artist.Text=t.Artist;
+			//set musicTitle
+			title.Text=t.Title;
+			//set AlbumTitleAndMusicCover
+			string[] ar=t.Tag.Split('|');
+			musicCover.Opacity=0;
+			musicCover.ImageSource=new BitmapImage(new Uri(ar[1] , UriKind.Absolute));
+			//图像淡入
+			musicCover.ImageOpened+=musicCover_ImageOpened;
+			//set topic
+			topic.Text="Vol."+ar[0]+" "+ar[2];
+			//set currentAlbumNumber
+			if(maxAlbumNumber==-1) maxAlbumNumber=int.Parse(ar[0]);
+			currentAlbumNumber=int.Parse(ar[0]);
+		}
+
+		private void UnableAllButton()
+		{
+			playAndPauseButton.IsEnabled=false;
+			lastAlbumButton.IsEnabled=false;
+			nextAlbumButton.IsEnabled=false;
+			nextSongButton.IsEnabled=false;
+			lastSongButton.IsEnabled=false;
+		}
+
+		private void EnableAllButton()
+		{
+			playAndPauseButton.IsEnabled=true;
+			lastAlbumButton.IsEnabled=true;
+			nextAlbumButton.IsEnabled=true;
+			nextSongButton.IsEnabled=true;
+			lastSongButton.IsEnabled=true;
+		}
+
+		void albumCover_ImageOpened( object sender , RoutedEventArgs e )
+		{
+			albumCoverStoryBoard.Begin();
+			albumCover.Opacity=0.8;
+		}
+
+		void musicCover_ImageOpened( object sender , RoutedEventArgs e )
+		{
+			musicCoverStoryBoard.Begin();
+			musicCover.Opacity=0.8;
+		}
+
+		void Instance_PlayStateChanged( object sender , EventArgs e )
+		{
+			bool t=false;
+			t=BackgroundAudioPlayer.Instance.Track.Title.Equals("error");
+			if(!t)
+			{
+				var state=e as PlayStateChangedEventArgs;
+				if(state.CurrentPlayState==PlayState.Playing)
+				{
+					playAndPause.Source=new BitmapImage(new Uri("/assets/icons/pause.png" , UriKind.Relative));
+				}
+				else
+				{
+					playAndPause.Source=new BitmapImage(new Uri("/assets/icons/play.png" , UriKind.Relative));
+				}
+				UpdateUI();
+				EnableAllButton();
+				progressBar.Visibility=Visibility.Collapsed;
+				isAlbumChanged=false;
+			}
+			else
+			{
+				progressBar.Visibility=Visibility.Collapsed;
+				BackgroundAudioPlayer.Instance.Close();
+				ToastPrompt toast=new ToastPrompt();
+				toast.Message="网络不可用，请检查网络连接";
+				toast.Show();
+			}
+		}
+		#endregion
+
+		#region Override Methods
+		protected override void OnNavigatedTo( NavigationEventArgs e )
+		{
+			ToastPrompt toast=new ToastPrompt();
+			if(DeviceNetworkInformation.IsNetworkAvailable)
+			{
+				if(BackgroundAudioPlayer.Instance.Track==null)
+				{
+					progressBar.Visibility=Visibility.Visible;
+					UnableAllButton();
+					BackgroundAudioPlayer.Instance.Play();
+				}
+				else
+				{
+					UpdateUI();
+				}
+			}
+			else
+			{
+				toast.Message="当前网络不可用";
+				toast.Show();
+			}
+			base.OnNavigatedTo(e);
+		}
+
+		protected override void OnBackKeyPress( System.ComponentModel.CancelEventArgs e )
+		{
+			if(!isExit)
+			{
+				isExit=true;
+				textStoryBoard.Begin();
+				Task.Factory.StartNew(() => {
+					Thread.Sleep(2000);
+					this.Dispatcher.BeginInvoke(() => isExit=false);
+				});
+				e.Cancel=true;
+			}
+			else
+			{
+				MessageBoxResult result=MessageBox.Show("停止播放音乐？" , "退出" , MessageBoxButton.OKCancel);
+				if(result==MessageBoxResult.OK)
+				{
+					BackgroundAudioPlayer.Instance.Close();
+				}
+				e.Cancel=false;
+			}
+		}
+		#endregion
+
+	}
+}
---
+++ LuooAudioPlayerAgent/AudioPlayer.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using Microsoft.Phone.BackgroundAudio;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Net;
+using System.IO;
+namespace LuooAudioPlayerAgent
+{
+	public class AudioPlayer : AudioPlayerAgent
+	{
+		#region 字段
+
+		static WebClient getMaxAlbumWebClient=new WebClient();
+		static WebClient getListWebClient=new WebClient();
+		static int currentTrackNumber=0;
+		static int maxAlbumNumber=-1;
+		static int currentAlbumNumber=-1;
+		static ManualResetEvent manulResetEvent=new ManualResetEvent(false);
+		private static List<AudioTrack> playList=new List<AudioTrack>();
+
+		#endregion
+
+		#region Constructor and error code
+		static AudioPlayer()
+		{
+			getMaxAlbumWebClient.OpenReadCompleted+=GetMaxAlbumNumber_OpenReadCompleted;
+			getListWebClient.OpenReadCompleted+=GetListWebClient_OpenReadCompleted;
+			Deployment.Current.Dispatcher.BeginInvoke(delegate
+			{
+				Application.Current.UnhandledException += UnhandledException;
+			});
+		}
+
+		#region 出现未处理的异常时执行的代码
+		private static void UnhandledException( object sender , ApplicationUnhandledExceptionEventArgs e )
+		{
+			if(Debugger.IsAttached)
+			{
+				// 出现未处理的异常；强行进入调试器
+				Debugger.Break();
+			}
+		}
+		#endregion
+		#endregion
+
+		#region override AudioPlayerAgent
+
+		protected override void OnPlayStateChanged( BackgroundAudioPlayer player , AudioTrack track , PlayState playState )
+		{
+			switch(playState)
+			{
+				case PlayState.TrackEnded:
+					PlayNextTrack(player);
+					break;
+				case PlayState.TrackReady:
+					player.Play();
+					break;
+
+			}
+			NotifyComplete();
+		}
+		protected override void OnUserAction( BackgroundAudioPlayer player , AudioTrack track , UserAction action , object param )
+		{
+			switch(action)
+			{
+				case UserAction.Play:
+					if(maxAlbumNumber==-1)
+					{
+						try
+						{
+							playList.Clear();
+							GetMaxAlbumNumber();
+							GetList();
+						}
+						catch
+						{
+							playList.Clear();
+							//todo..
+							playList.Add(new AudioTrack(new Uri("http://www.baidu.com") , "error" , null , null , null));
+						}
+						player.Track=playList[currentTrackNumber];
+					}
+					if(player.Track!=null) player.Play();
+					break;
+
+				case UserAction.Pause:
+					player.Pause();
+					break;
+
+				case UserAction.SkipNext:
+					PlayNextTrack(player);
+					break;
+
+				case UserAction.SkipPrevious:
+					PlayPreviousTrack(player);
+					break;
+				case UserAction.FastForward:
+					if(currentAlbumNumber<maxAlbumNumber)
+					{
+						try
+						{
+							++currentAlbumNumber;
+							GetList();
+						}
+						catch
+						{
+							--currentAlbumNumber;
+							//todo...
+							playList.Add(new AudioTrack(new Uri("http://www.baidu.com") , "error" , null , null , null));
+						}
+						player.Track=playList[currentTrackNumber];
+					}
+					break;
+				case UserAction.Rewind:
+					try
+					{
+						--currentAlbumNumber;
+						GetList();
+					}
+					catch
+					{
+						++currentAlbumNumber;
+						//todo..
+						playList.Add(new AudioTrack(new Uri("http://www.baidu.com") , "error" , null , null , null));
+					}
+					player.Track=playList[currentTrackNumber];
+					break;
+				case UserAction.Stop:
+					break;
+			}
+			NotifyComplete();
+		}
+		protected override void OnError( BackgroundAudioPlayer player , AudioTrack track , Exception error , bool isFatal )
+		{
+			if(isFatal)
+			{
+				Abort();
+			}
+			else
+			{
+				NotifyComplete();
+			}
+
+		}
+		protected override void OnCancel()
+		{
+		}
+
+		#endregion
+
+		#region NextTrack And PreviousTrack
+
+		private void PlayNextTrack( BackgroundAudioPlayer player )
+		{
+			if(++currentTrackNumber >=playList.Count)
+			{
+				currentTrackNumber = 0;
+			}
+			player.Track=playList[currentTrackNumber];
+		}
+
+		private void PlayPreviousTrack( BackgroundAudioPlayer player )
+		{
+
+			if(--currentTrackNumber < 0)
+			{
+				currentTrackNumber = playList.Count - 1;
+			}
+			player.Track=playList[currentTrackNumber];
+
+		}
+
+		#endregion
+
+		#region 获取当前期数新播放列表
+		public static void GetList()
+		{
+			playList.Clear();
+			if(maxAlbumNumber!=-1)
+			{
+				getListWebClient.OpenReadAsync(new Uri("http://www.luoo.net/music/"+currentAlbumNumber));
+				manulResetEvent.Reset();
+				manulResetEvent.WaitOne();
+			}
+			else throw new WebException();
+		}
+		static void GetListWebClient_OpenReadCompleted( object sender , OpenReadCompletedEventArgs e )
+		{
+			try
+			{
+				StreamReader sR=new StreamReader(e.Result);
+				string html=sR.ReadToEnd();
+				int playListCount;
+				string pattern;
+				MatchCollection matches;
+				AudioTrack t;
+
+				#region getAlbumAndArtist
+				pattern=@"track-album.>(.+)-(.+)</a>";
+				matches=Regex.Matches(@html , pattern);
+				playListCount=matches.Count;
+				for(int i=0 ; i<playListCount ; i++)
+				{
+					t=new AudioTrack();
+					t.BeginEdit();
+					t.Album=matches[i].Groups[1].ToString();
+					t.Artist=matches[i].Groups[2].ToString();
+					t.EndEdit();
+					playList.Add(t);
+				}
+				#endregion
+
+				#region getList
+				pattern=@"http\S+01.mp3";
+				Match matche=Regex.Match(html , pattern);
+				string title=matche.ToString().Replace(@"\" , null);
+				title=title.Remove(title.Length-6);
+				for(int i=0 ; i<playListCount ; i++)
+				{
+					playList[i].BeginEdit();
+					if(i+1<10)
+						playList[i].Source=new Uri(title+"0"+(i+1).ToString()+".mp3");
+					else
+						playList[i].Source=new Uri(title+(i+1).ToString()+".mp3");
+					playList[i].EndEdit();
+				}
+				#endregion
+
+				#region getMusicTitleAndMusicCover
+				pattern=@"img alt=.(.+). src=.(http://img.luoo.net/\S+60x60.jpg)";
+				matches=Regex.Matches(@html , pattern);
+				for(int i=0 ; i<playListCount ; i++)
+				{
+					t=playList[i];
+					t.BeginEdit();
+					t.Title=matches[i].Groups[1].ToString();
+					t.Tag=currentAlbumNumber+"|"+matches[i].Groups[2].ToString();
+					t.EndEdit();
+				}
+				#endregion
+
+				#region getAlbumCover
+				pattern=@"http://img.luoo.net/\S+640x452.jpg";
+				matches=Regex.Matches(@html , pattern);
+				Uri cover=new Uri(matches[0].ToString() , UriKind.Absolute);
+				for(int i=0 ; i<playListCount ; i++)
+				{
+					playList[i].BeginEdit();
+					playList[i].AlbumArt=cover;
+					playList[i].EndEdit();
+				}
+				#endregion
+
+				#region getAlbumTitle
+				pattern=@"<title>(\S+)</title>";
+				matches=Regex.Matches(@html , pattern);
+				string temp="|"+matches[0].Groups[1].ToString();
+				for(int i=0 ; i<playListCount ; i++)
+				{
+					playList[i].BeginEdit();
+					playList[i].Tag+=temp;
+					playList[i].EndEdit();
+				}
+				#endregion
+
+				//
+				sR.Close();
+			}
+			catch
+			{
+				//throw new Exception();
+				playList.Add(new AudioTrack(new Uri("http://www.baidu.com") , "error" , null , null , null));
+			}
+			currentTrackNumber=0;
+			manulResetEvent.Set();
+		}
+		#endregion
+
+		#region 获取当前最大的期数
+		public static void GetMaxAlbumNumber()
+		{
+			getMaxAlbumWebClient.OpenReadAsync(new Uri("http://www.luoo.net"));
+			//wait...
+			manulResetEvent.WaitOne();
+		}
+		static void GetMaxAlbumNumber_OpenReadCompleted( object sender , OpenReadCompletedEventArgs e )
+		{
+			try
+			{
+				//if(e.Error!=null) throw e.Error;
+				StreamReader sR=new StreamReader(e.Result);
+				//close
+				string html=sR.ReadToEnd();
+				//GetMaxAlbumNumber
+				string pattern=@"vol. [0-9]+";
+				MatchCollection matches=Regex.Matches(@html , pattern);
+				string temp=matches[0].ToString().Split(' ')[1];
+				maxAlbumNumber=int.Parse(temp);
+				//set currentAlbumNumber
+				currentAlbumNumber=maxAlbumNumber;
+				//
+				sR.Close();
+			}
+			catch(Exception ex)
+			{
+				//throw ex;
+			}
+			manulResetEvent.Set();
+		}
+		#endregion
+
+
+	}
+}
